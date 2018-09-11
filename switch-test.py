1fb959b502f382d71dd0888b72127d2e2de7ab35
Kevin Mettenberger <kevin@mettenberger.ch>
2018-09-11 09:43:50 +0200

    adds switch-test

---
+++ switch-test.py
@@ -0,0 +1,14 @@
+import sys
+import time
+import RPi.GPIO as GPIO
+
+
+input=17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(input, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")
