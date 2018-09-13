8ad9f35ed10ce220eae4c9e3a5ddd0fcd88bd447
Kevin Mettenberger <kevin@mettenberger.ch>
2018-09-13 17:17:40 +0200

    - adapt rest messages to new ci - add motor speed - change pid sample size to 1 second

--- iot-demo.py
+++ iot-demo.py
@@ -31,7 +31,7 @@ GAIN = 2
 
 #PID setup
 pid = PID(1, 0.1, 0.05, setpoint=440)
-pid.sample_time = 2
+pid.sample_time = 1
 pid.output_limits = (30, 90)
 
 #Switch Setup
@@ -72,8 +72,9 @@ switch1On = False
 pid.auto_mode = False
 
 lowestValue = 450
-avg = 450
-valueArray = []
+windAvg = 450
+windValueArray = []
+motorValueArray = []
 numberOfReadings = 20
 
 headers = {'Content-Type': 'application/json'}
@@ -97,28 +98,41 @@ while(1):
 
     if switch1On == True:
         value = adc.read_adc(0, gain=GAIN)
-        valueArray.append(value)
-        avg = mean(valueArray)
+        windValueArray.append(value)
+        windAvg = mean(windValueArray)
 
         if len(valueArray) == numberOfReadings:
             valueArray.pop(0)
 
 
         control = pid(value)
+        motorValueArray.append(control)
+        motorAvg = mean(motorValueArray)
         speedControl.ChangeDutyCycle(control)
 
     if minutePassed():
-        print(avg)
+        print("Wind: "+str(windAvg))
+        print("Motor: "+str(motorAvg))
         print("Minute passed")
         currentDateTime = datetime.datetime.now().replace(microsecond=0).isoformat()
-        data = '''{"seriesRef":
-            {"subject":"4e7874a8dbe8e700c58f7a6eaf9619c7",
-                    "table":"u_cmdb_ci_machinery",
-                    "metric":"u_motor_speed"},
+        windData = '''{"seriesRef":
+            {"subject":"1a8ed349dba46b00c58f7a6eaf9619e1",
+                    "table":"x_snc_ma_maint_u_cmdb_ci_machine",
+                    "metric":"wind_speed"},
             "values":[
-                    {"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(avg)+'''}
+                    {"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(windAvg)+'''}
                     ]
             }'''
-        resp = requests.post(url, data=data, headers=headers, auth=('iot_demo', 'SNIoT1234'))
+        resp = requests.post(url, data=windData, headers=headers, auth=('iot_demo', 'SNIoT1234'))
+
+        windData = '''{"seriesRef":
+            {"subject":"1a8ed349dba46b00c58f7a6eaf9619e1",
+                    "table":"x_snc_ma_maint_u_cmdb_ci_machine",
+                    "metric":"motor_speed"},
+            "values":[
+                    {"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(motorAvg)+'''}
+                    ]
+            }'''
+        resp = requests.post(url, data=windData, headers=headers, auth=('iot_demo', 'SNIoT1234'))
 
     time.sleep(0.5)
