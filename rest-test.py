93419100490ac4f6219a0e57d4c6aee673b4b42e
Kevin Mettenberger <kevin@mettenberger.ch>
2018-09-13 16:20:18 +0200

    - update for second ci

--- rest-test.py
+++ rest-test.py
@@ -7,8 +7,10 @@ import random
 import datetime
 
 
-randomNumber = random.randint(435,450)
-random2 = random.randint(40,50)
+windSpeed = random.randint(435,450)
+motorSpeed = random.randint(40,50)
+windSpeed2 = random.randint(435,450)
+motorSpeed2 = random.randint(40,50)
 print(randomNumber)
 currentDateTime = datetime.datetime.now().replace(microsecond=0).isoformat()
 #ms = int(round(time.time() * 1000))
@@ -22,7 +24,29 @@ data = '''{"seriesRef":
 "table":"x_snc_ma_maint_u_cmdb_ci_machine",
 "metric":"wind_speed"},
 "values":[
-{"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(randomNumber)+'''}
+{"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(windSpeed)+'''}
+]
+}'''
+
+resp = requests.post(url, data=data, headers=headers, auth=('iot_demo', 'SNIoT1234'))
+
+data2 = '''{"seriesRef":
+{"subject":"2b1b9c15db686b00c58f7a6eaf9619aa",
+"table":"x_snc_ma_maint_u_cmdb_ci_machine",
+"metric":"motor_speed"},
+"values":[
+{"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(motorSpeed)+'''}
+]
+}'''
+
+resp = requests.post(url, data=data2, headers=headers, auth=('iot_demo', 'SNIoT1234'))
+
+data = '''{"seriesRef":
+{"subject":"2b1b9c15db686b00c58f7a6eaf9619aa",
+"table":"x_snc_ma_maint_u_cmdb_ci_machine",
+"metric":"wind_speed"},
+"values":[
+{"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(windSpeed2)+'''}
 ]
 }'''
 
@@ -33,7 +57,7 @@ data2 = '''{"seriesRef":
 "table":"x_snc_ma_maint_u_cmdb_ci_machine",
 "metric":"motor_speed"},
 "values":[
-{"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(random2)+'''}
+{"timestamp":"'''+str(currentDateTime)+'''Z", "value":'''+str(motorSpeed2)+'''}
 ]
 }'''
 
